754ff04e3e78b4d041d553aa762d833db138e09c
ananya2511mahe <96558915+ananya2511mahe@users.noreply.github.com>
2022-02-01 07:04:29 +0000

    p1original.c completed

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
-
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter a numbers/n");
+  scanf("%d",&a);
+  return a;
+}
+int add (int a, int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+void output(int a, int b, int c)
+{
+  printf("%d+ %d is %d\n",a,b,c);
+}
+int main()
+{
+  int a=input ();
+  int b=input();
+  int c=add(a,b);
+  output(a,b,c);
+  return 0;
 }
